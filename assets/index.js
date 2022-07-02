0bbd044c9e07fd76b0498516f91c5dc810756ae4
Sergey <frontierguard94@gmail.com>
2022-07-02 19:26:52 +0300

    burger

--- assets/index.js
+++ assets/index.js
@@ -2,12 +2,14 @@ const burgerBtn = document.getElementById('burger__btn')
 let burgerMenu = document.querySelector('.header__wrapper')
 burgerBtn.addEventListener('click', function () {
 	burgerMenu.classList.toggle('active')
+	burgerBtn.classList.toggle('burger__active')
 })
 
 const footerBurgerBtn = document.getElementById('footer__burger__btn')
 let footerBurgerMenu = document.querySelector('.footer__wrapper')
 footerBurgerBtn.addEventListener('click', function () {
 	footerBurgerMenu.classList.toggle('active')
+	footerBurgerBtn.classList.toggle('burger__active')
 })
 
 // const galleryItem1 = document.getElementById('gallery__img-1')
